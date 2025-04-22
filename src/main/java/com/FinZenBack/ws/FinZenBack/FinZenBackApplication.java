ba2f9f1344c785a778fe07a361ae1978a4a3c946
Andres Sarchi <andressarchi@MacBook-Air-de-Andres.local>
2025-04-22 08:32:47 -0500

    creacion de carpetas del back

---
+++ src/main/java/com/FinZenBack/ws/FinZenBack/FinZenBackApplication.java
@@ -0,0 +1,13 @@
+package com.FinZenBack.ws.FinZenBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinZenBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinZenBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FinZenBack/ws/FinZenBack/FinZenBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FinZenBack.ws.FinZenBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinZenBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
