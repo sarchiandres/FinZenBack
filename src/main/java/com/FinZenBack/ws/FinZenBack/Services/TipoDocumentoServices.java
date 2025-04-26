521236c5e48a1762d4dd4fa4821721a05cc6c1b1
sarchiandres <sarchiandres63@gmail.com>
2025-04-26 10:13:34 -0500

    se crea las entidades de usuaario,tipo usuario ,tipo documento y sus controladores

---
+++ src/main/java/com/FinZenBack/ws/FinZenBack/Services/TipoDocumentoServices.java
@@ -0,0 +1,23 @@
+package com.FinZenBack.ws.FinZenBack.Services;
+
+import com.FinZenBack.ws.FinZenBack.models.TipoDocumento;
+import com.FinZenBack.ws.FinZenBack.repository.TipoDocumentoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TipoDocumentoServices {
+    private final TipoDocumentoRepository TDrepository;
+
+    public TipoDocumentoServices(TipoDocumentoRepository tDrepository) {
+        TDrepository = tDrepository;
+    }
+
+    public List<TipoDocumento> getTipos (){
+        return TDrepository.findAll();
+    }
+    public TipoDocumento createTipo(TipoDocumento tipoDocumento){
+        return TDrepository.save(tipoDocumento);
+    }
+}
