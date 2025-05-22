621c5adff8970d0d986b06251a7f1051238395c5
Julian Cortes <mmm808383@gmail.com>
2025-05-22 06:16:45 -0500

    Cambios Basicos para el uso

--- src/main/java/com/FinZenBack/ws/FinZenBack/payload/LoginRequest.java
+++ src/main/java/com/FinZenBack/ws/FinZenBack/payload/LoginRequest.java
@@ -13,4 +13,29 @@ public class LoginRequest {
 
     @NotBlank
     private String contrasena;
+
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
 }
